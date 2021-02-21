412dd9630a8b29fca319e3dc4a69c70fa6b63374
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-02-20 18:53:54 -0800

    Initial Commit

---
+++ schoologywebcontroller/__init__.py
@@ -0,0 +1,4 @@
+from .schoologywebcontroller import SchoologyController
+
+__version__ = '0.0.1'
+__author__ = 'Joe Aguilar'
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='schoologywebcontroller',
+    version='0.0.1',
+    description='A base package for my web controllers.',
+    url='https://github.com/GitPushPullLegs/schoologywebcontroller',
+    author='Joe Aguilar',
+    author_email='jose.aguilar.6694@gmail.com',
+    license='MIT License',
+    packages=find_packages(exclude=[]),
+    install_requires=['basewebcontroller @ git+https://github.com/GitPushPullLegs/basewebcontroller.git',
+                      'python-dateutil',
+                      ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
